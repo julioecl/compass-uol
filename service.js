4204fa4a483c13b43caba624e2257cc4bf90ffbf
Julio Lima <julioecl@hotmail.com>
2023-01-27 14:31:02 -0300

    Primeiro commit
    adicionando todos os arquivos ao repositório do GitHub

---
+++ service.js
@@ -0,0 +1,13 @@
+function somar(a, b){
+    resultado = a + b
+    return resultado
+}
+
+console.log(somar(12, 15))
+
+function multiplicar(a, b){
+    resultado = a * b
+    return resultado
+}
+
+console.log(multiplicar(2, 5))
